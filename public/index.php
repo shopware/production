dd6ef1933b7754aacd1cf162325a04887341621c
Patrick Weyck <p.weyck@shopware.com>
2019-12-03 11:17:58 +0100

    NEXT-5613 - Add recovery entrypoint

--- public/index.php
+++ public/index.php
@@ -14,6 +14,30 @@ use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 
 $classLoader = require __DIR__.'/../vendor/autoload.php';
 
+if (!file_exists(dirname(__DIR__) . '/install.lock')) {
+    $basePath = 'recovery/install';
+    $baseURL = str_replace(basename(__FILE__), '', $_SERVER['SCRIPT_NAME']);
+    $baseURL = rtrim($baseURL, '/');
+    $installerURL = $baseURL . '/' . $basePath . '/index.php';
+    if (strpos($_SERVER['REQUEST_URI'], $basePath) === false) {
+        header('Location: ' . $installerURL);
+        exit;
+    }
+}
+
+if (is_file(dirname(__DIR__) . '/files/update/update.json') || is_dir(dirname(__DIR__) . '/update-assets')) {
+    header('Content-type: text/html; charset=utf-8', true, 503);
+    header('Status: 503 Service Temporarily Unavailable');
+    header('Retry-After: 1200');
+    if (file_exists(__DIR__ . '/maintenance.html')) {
+        readfile(__DIR__ . '/maintenance.html');
+    } else {
+        readfile(__DIR__ . '/recovery/update/maintenance.html');
+    }
+
+    return;
+}
+
 // The check is to ensure we don't use .env if APP_ENV is defined
 if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
