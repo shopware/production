5008beb03f969552e6952452127ccaa7f8f7d22c
Joshua Behrens <behrens@heptacom.de>
2020-02-17 10:08:28 +0100

    Require more steps for building storefront
    fixes https://github.com/shopware/production/pull/13

--- bin/build-storefront.sh
+++ bin/build-storefront.sh
@@ -6,6 +6,8 @@ export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname $CWD)"}"
 STOREFRONT_ROOT="${STOREFRONT_ROOT:-"${PROJECT_ROOT}/vendor/shopware/storefront"}"
 
 # build storefront
+"${CWD}/console" bundle:dump
 npm --prefix ${STOREFRONT_ROOT}/Resources/app/storefront clean-install
 node ${STOREFRONT_ROOT}/Resources/app/storefront/copy-to-vendor.js
 npm --prefix ${STOREFRONT_ROOT}/Resources/app/storefront run production
+"${CWD}/console" asset:install
