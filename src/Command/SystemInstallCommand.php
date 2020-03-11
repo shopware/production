35c24c357ce6bd7275aa6f0e07e2bd7140579db7
Joshua Behrens <behrens@heptacom.de>
2020-03-11 20:55:36 +0100

    Add command to change system language
    Relates to #2

--- src/Command/SystemInstallCommand.php
+++ src/Command/SystemInstallCommand.php
@@ -41,7 +41,8 @@ class SystemInstallCommand extends Command
         $this->addOption('create-database', null, InputOption::VALUE_NONE, "Create database if it doesn't exist.")
             ->addOption('drop-database', null, InputOption::VALUE_NONE, 'Drop existing database')
             ->addOption('basic-setup', null, InputOption::VALUE_NONE, 'Create storefront sales channel and admin user')
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force install even if install.lock exists');
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force install even if install.lock exists')
+            ->addOption('locale', 'l', InputOption::VALUE_REQUIRED, 'Shop locale in iso format');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -132,6 +133,18 @@ class SystemInstallCommand extends Command
                 'command' => 'theme:compile',
             ],
         ];
+
+        if (!empty($input->getOption('locale'))) {
+            $this->getApplication()->find('system:locale-destructive')->activateCommand();
+            $commands[] = [
+                'command' => 'system:locale-destructive',
+                'locale' => $input->getOption('locale'),
+            ];
+
+            $commands[] = [
+                'command' => 'cache:clear',
+            ];
+        }
 
         if ($input->getOption('basic-setup')) {
             $commands[] = [
