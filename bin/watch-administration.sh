3f06ee41a4e1197039669ed23fe2b2d86795b84f
Dominik Mank <d.mank@bucs-it.de>
2020-09-07 15:05:22 +0200

    escape the source variable, so that directories with spaces can be used

--- bin/watch-administration.sh
+++ bin/watch-administration.sh
@@ -5,7 +5,7 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname $CWD)"}"
 export ENV_FILE=${ENV_FILE:-"${PROJECT_ROOT}/.env"}
 
-source ${ENV_FILE}
+source "${ENV_FILE}"
 export HOST=${HOST:-"localhost"}
 export ESLINT_DISABLE
 export PORT
--- bin/watch-storefront.sh
+++ bin/watch-storefront.sh
@@ -5,7 +5,7 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname $CWD)"}"
 export ENV_FILE=${ENV_FILE:-"${PROJECT_ROOT}/.env"}
 
-source ${ENV_FILE}
+source "${ENV_FILE}"
 export APP_URL
 export STOREFRONT_PROXY_PORT
 export ESLINT_DISABLE
