8e41e81246b7e3fe3434bcf5b6b2084a0b4010e6
Soner Sayakci <s.sayakci@shopware.com>
2021-07-14 19:14:33 +0000

    NEXT-16218 - Auto install npm dependencies of extensions

--- bin/build-administration.sh
+++ bin/build-administration.sh
@@ -9,5 +9,28 @@ ADMIN_ROOT="${ADMIN_ROOT:-"${PROJECT_ROOT}/vendor/shopware/administration"}"
 
 # build admin
 [[ ${CI} ]] || "${CWD}/console" bundle:dump
+
+if [[ `command -v jq` ]]; then
+    OLDPWD=$(pwd)
+    cd $PROJECT_ROOT
+
+    jq -c '.[]' "var/plugins.json" | while read config; do
+        srcPath=$(echo $config | jq -r '(.basePath + .administration.path)')
+
+        # the package.json files are always one upper
+        path=$(dirname $srcPath)
+        name=$(echo $config | jq -r '.technicalName' )
+
+        if [[ -f "$path/package.json" && ! -f "$path/node_modules" && $name != "administration" ]]; then
+            echo "=> Installing npm dependencies for ${name}"
+
+            npm install --prefix "$path"
+        fi
+    done
+    cd "$OLDPWD"
+else
+    echo "Cannot check extensions for required npm installations as jq is not installed"
+fi
+
 (cd "${ADMIN_ROOT}"/Resources/app/administration && npm clean-install && npm run build)
 [[ ${CI} ]] || "${CWD}/console" asset:install
--- bin/build-storefront.sh
+++ bin/build-storefront.sh
@@ -7,6 +7,30 @@ STOREFRONT_ROOT="${STOREFRONT_ROOT:-"${PROJECT_ROOT}/vendor/shopware/storefront"
 
 # build storefront
 [[ ${CI} ]] || "${CWD}/console" bundle:dump
+
+if [[ `command -v jq` ]]; then
+    OLDPWD=$(pwd)
+    cd $PROJECT_ROOT
+
+    jq -c '.[]' "var/plugins.json" | while read config; do
+        srcPath=$(echo $config | jq -r '(.basePath + .storefront.path)')
+
+        # the package.json files are always one upper
+        path=$(dirname $srcPath)
+        name=$(echo $config | jq -r '.technicalName' )
+
+        if [[ -f "$packageJsonPath/package.json" && ! -f "$packageJsonPath/node_modules" && $name != "storefront" ]]; then
+            echo "=> Installing npm dependencies for ${name}"
+
+            npm install --prefix "$packageJsonPath"
+        fi
+    done
+    cd "$OLDPWD"
+else
+    echo "Cannot check extensions for required npm installations as jq is not installed"
+fi
+
+
 npm --prefix "${STOREFRONT_ROOT}"/Resources/app/storefront clean-install
 node "${STOREFRONT_ROOT}"/Resources/app/storefront/copy-to-vendor.js
 npm --prefix "${STOREFRONT_ROOT}"/Resources/app/storefront run production
