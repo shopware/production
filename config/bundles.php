633643e81b0113bbcc7d7be44384bf5a06e6b51b
Julian Krzefski <krzefski@heptacom.de>
2022-03-21 21:58:45 +0100

    NEXT-22513 - Improve compatibility with composer install --no-dev fixes #145

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ $bundles = [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
     Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
@@ -13,18 +12,22 @@ $bundles = [
     Shopware\Core\System\System::class => ['all' => true],
     Shopware\Core\Content\Content::class => ['all' => true],
     Shopware\Core\Checkout\Checkout::class => ['all' => true],
-    Shopware\Core\Profiling\Profiling::class => ['dev' => true],
     Shopware\Administration\Administration::class => ['all' => true],
     Shopware\Storefront\Storefront::class => ['all' => true],
     Shopware\Elasticsearch\Elasticsearch::class => ['all' => true],
 ];
 
-if (\class_exists('Shopware\Core\Maintenance\Maintenance')) {
+if (\class_exists(Shopware\Core\Maintenance\Maintenance::class)) {
     $bundles[Shopware\Core\Maintenance\Maintenance::class] = ['all' => true];
 }
 
 if (\class_exists(Shopware\Core\DevOps\DevOps::class)) {
     $bundles[Shopware\Core\DevOps\DevOps::class] = ['e2e' => true];
+}
+
+if (\class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
+    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'test' => true];
+    $bundles[Shopware\Core\Profiling\Profiling::class] = ['dev' => true];
 }
 
 return $bundles;
