6e1a14f5a3a3bd23299f5cf6dd8464c5fcc16976
Soner Sayakci <s.sayakci@shopware.com>
2022-09-20 11:20:07 +0200

    NEXT-21195 - Cleanup template

--- bin/build-js.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BIN_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-
-set -euo pipefail
-
-"${BIN_DIR}/build-administration.sh"
-"${BIN_DIR}/build-storefront.sh"
